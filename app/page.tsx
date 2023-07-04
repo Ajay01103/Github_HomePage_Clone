925a1ba09d063749e8fd0363f4caab6b9ec8e5e1
Ajay Singh <125455606+Ajay01103@users.noreply.github.com>
2023-07-04 14:56:45 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,21 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="text-3xl">
-      Github HomePage Clone Start From app/page.tsx
-    </main>
+    <div>
+      <div className='relative z-50'>
+        <div className='absolute'>
+
+        </div>
+      </div>
+      <div>
+        <div className='overflow-hidden'>
+          <div className='relative h-screen'>
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" width='4377' alt="back" 
+            className=' absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] image'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
