cee553278cad0da82ec2ee48d78eb38fce83e7ed
Ajay01103 <ajaysingh01103@gmail.com>
2023-08-11 15:54:03 +0530

    initial commit

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Container from '@/app/Container'
+import ProductNav from '@/app/Components/Navbar/ProductNav'
 import React, { useState } from 'react'
 
 type Props = {}
@@ -28,7 +29,11 @@ const Navbar = (props: Props) => {
                </div>
                <div className={`flex w-full max-lg:absolute transition ease-in duration-200 top-20 z-40 ${showNav ? " max-lg:translate-x-0 max-lg:scale-100" : "max-lg:max-h-0 max-lg:scale-y-50 max-lg:translate-y-1/4"}`}>
                 <div className='flex max-lg:flex-col lg:items-center lg:justify-between max-lg:justify-between max-sm:w-10/12 max-sm:mx-auto sm:ml-auto max-lg:w-[320px] lg:w-full max-lg:max-h-[500px] overflow-y-auto lg:mb-0 max-lg:bg-white max-lg:rounded-lg'>
-
+                  <nav className=' mt-0 px-3 lg:px-6 text-[15.5px] mb-0 max-lg:py-10 max-lg:px-5'>
+                    <ul className=' flex lg:items-center lg:space-x-2 max-lg:flex-col max-lg:text-neutral-700 max-lg:text-xl max-lg:font-medium max-lg:space-y-6 max-lg:h-auto overflow-y-auto'>
+                      <ProductNav />
+                    </ul>
+                  </nav>
                 </div>
                </div>
             </div>
